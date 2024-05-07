d98928f70771364c26a9fe8ed7d74fd5cb199cfc
Luke Gessler <lukegessler@gmail.com>
2024-05-06 20:48:12 -0400

    reduce num proc

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-tokenized_datasets = dataset.map(tokenize_function, batched=True, num_proc=12)
+tokenized_datasets = dataset.map(tokenize_function, batched=True, num_proc=4)
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
 small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
